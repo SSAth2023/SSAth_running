9bfa0785067cc0385c850d29354cde1d29e14bb4
imin <statistics.m94@gmail.com>
2023-11-14 17:45:00 +0900

    20231114_m2

--- Sql/SSAth_db.sql
+++ Sql/SSAth_db.sql
@@ -10,32 +10,27 @@ CREATE TABLE IF NOT EXISTS User (
 
 CREATE TABLE IF NOT EXISTS RunningPath (   
 	map_id INT AUTO_INCREMENT PRIMARY KEY,   
-    user_id VARCHAR(20) REFERENCES User(user_id),   
+    user_id VARCHAR(20) REFERENCES User(user_id) ON DELETE CASCADE,   
     `path` TEXT,   
     `description` VARCHAR(3000),
     distance INT,   
     reg_date DATETIME 
 );
-DROP TABLE COMMENT;
+
 CREATE TABLE IF NOT EXISTS Comment (   
 	comment_id INT AUTO_INCREMENT,
-    map_id INT REFERENCES RunningPath(map_id),
-    user_id VARCHAR(20) REFERENCES User(user_id),   
+    map_id INT REFERENCES RunningPath(map_id) ON DELETE CASCADE,
+    user_id VARCHAR(20) REFERENCES User(user_id) ON DELETE CASCADE,   
     content VARCHAR(3000), 
     com_reg_date DATETIME,
     CONSTRAINT comment_PK PRIMARY KEY(comment_id, map_id)
 );
 
-SELECT * FROM COMMENT;
-INSERT INTO COMMENT VALUES(0, 0,1,1,1,"2022-02-22");
-INSERT INTO COMMENT VALUES(0,0,2,2,1,"2022-02-22");
-INSERT INTO COMMENT VALUES(0,0,1,1,1,"2022-02-22");
-
 CREATE TABLE IF NOT EXISTS Bookmark (
 	map_id INT,
 	user_id VARCHAR(20),
     marked BOOLEAN,
-    FOREIGN KEY (map_id) REFERENCES RunningPath(map_id),
-	FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (map_id) REFERENCES RunningPath(map_id) ON DELETE CASCADE,
+	FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
     CONSTRAINT Bookmark_PK PRIMARY KEY(map_id, user_id)
 );
