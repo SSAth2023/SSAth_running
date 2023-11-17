c02da0d7309d26adbb87a6939c88edcefaf17b0e
imin <statistics.m94@gmail.com>
2023-11-17 14:41:43 +0900

    20231117_m1_JwtUtil&JwtInterceptor

--- Spring/SsathRunning/src/main/java/com/ssath/map/config/WebConfig.java
+++ Spring/SsathRunning/src/main/java/com/ssath/map/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ssath.map.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -7,10 +8,19 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.ssath.map.interceptor.JwtInterceptor;
+
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	private JwtInterceptor jwtInterceptor; 
+	
+	@Autowired
+	public void setJwtInterceptor(JwtInterceptor jwtInterceptor) {
+		this.jwtInterceptor = jwtInterceptor;
+	}
 	
 	//스웨거 동작
 	@Override
@@ -24,7 +34,9 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		//등록 필요
+		registry.addInterceptor(jwtInterceptor)
+		.addPathPatterns("/api/bookmark", "/path/create", "/path/delete", "/path/update", "/comment/**")
+		.excludePathPatterns("/comment/list/**");
 	}
 	
 	//vue 요청에 대한 CORS 에러 해결
